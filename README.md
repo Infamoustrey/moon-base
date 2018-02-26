1851ce5c87ece951a17de2bf8ea015b6070667a2
Trey Foster <treybfoster@gmail.com>
2018-02-25 22:49:06 -0600

    Minor Update -- Fleshed out CLI tool -- Added CLI arguments, need to add more -- Make swanky logo -- Need to add parots

--- README.md
+++ README.md
@@ -1,17 +1,18 @@
 
-<p align="center">
-<img src="https://github.com/Infamoustrey/moon-base/blob/master/moon-base.png" width="250" />
-</p>
 
 # moon-base
 > A PHP/JS Scaffolding Module, written by Trey Foster
 
+<p align="center">
+<img src="https://github.com/Infamoustrey/moon-base/blob/master/moon-base.png" width="250" />
+</p>
+
 ## Install
 ```bash
-npm install -g moon-base
+$ npm install -g moon-base
 ```
 
 ## Usage
 ```bash
-moonbase init
+$ moonbase init
 ```
--- cli-tool.js
+++ cli-tool.js
@@ -1,12 +1,47 @@
 #!/usr/bin/env node
 
+const fs = require('fs');
+const sf = require('./scaffold.js');
+
 // arguments
 const optionArgs = [
-    { name: '', alias: 'v', type: Boolean, defaultValue: false }
+    { name: 'init', alias: 'i', type: Boolean, defaultValue: false },
+    { name: 'outline', alias: 'o', type: String },
 ];
 
 //
 const cli_args = require('command-line-args');
 const options = cli_args(optionArgs);
 
-console.log(options);
+if(options.init){
+
+      if(options.outline){
+
+          fs.readFile(options.outline, 'utf8', (err, data) => {
+
+              if(!options.outline.includes('.json') || err) {
+                  console.error("File is either wrong format or does not exist!");
+                  process.exit(1);
+              }
+
+              let structure = {};
+              try{
+                  structure = JSON.parse(data);
+              }catch(err){
+                  console.error('Bad Outline File');
+                  process.exit(1);
+              }
+
+              sf.scaffold(structure);
+
+          });
+
+      }else{
+        console.error('No Outline File!');
+        process.exit(1);
+      }
+
+}else{
+    console.log('Fly me to the moon...');
+    process.exit(1); 
+}
